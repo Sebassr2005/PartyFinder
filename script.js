9939a03f360579f2100491f7912ab8a3d6948805
Sebassr2005 <141794884+Sebassr2005@users.noreply.github.com>
2024-11-19 17:21:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ const nav = document.querySelector('nav');
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.add('show');
+        }, index * 300); // Retraso de 300ms entre cada tarjeta
+    });
+});
